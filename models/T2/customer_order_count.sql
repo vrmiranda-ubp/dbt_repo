379dd0dbe7ef83cb18c2e2ebbdf48efa354ffbae
vic.miranda00@gmail.com <vic.miranda00@gmail.com>
2025-11-17 01:48:25 +0000

    commit

--- models/T2/customer_order_count.sql
+++ models/T2/customer_order_count.sql
@@ -10,7 +10,7 @@
         FROM {{ source('T1', 'customer') }}
     )
     SELECT C_CUSTKEY
-        , SUM(O_TOTALPRICE) AS TOTAL_PRICE
+        , COUNT(DISTINCT ord.O_CUSTKEY) AS ORDER_COUNT
     FROM ord
     JOIN cust
     ON ord.O_CUSTKEY = cust.C_CUSTKEY
