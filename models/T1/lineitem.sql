a060b6d5b84dfff0b4ffd7880e4774ba974d1e97
vic.miranda00@gmail.com <vic.miranda00@gmail.com>
2025-11-18 03:39:46 +0000

    commit

--- models/T1/lineitem.sql
+++
@@ -1,8 +0,0 @@
--- lineitem.sql
-{{ config(materialized='table') }} -- or 'table' for a materialized table
-
-with source_lineitem as (
-    select *
-    from {{ source('T1', 'lineitem') }}
-)
-SELECT * FROM source_lineitem
