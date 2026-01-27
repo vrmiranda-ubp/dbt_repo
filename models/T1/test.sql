72590954b262cbed190b4cfc38fb25e2540e97e9
vic miranda <vic.miranda00@gmail.com>
2026-01-27 03:27:10 +0000

    -

--- models/T1/test.sql
+++ models/T1/test.sql
@@ -1,3 +1,10 @@
+{{
+    config(
+        materialized="incremental",
+        snowflake_warehouse='COMPUTE_WH'
+    )
+}}
+
 select 
     -- Inject the run id if present, otherwise use "manual"
     env_var("env_attrib") as _audit_run_id
