4d794da734d65953edc02f6c48726746f819853d
vic miranda <vic.miranda00@gmail.com>
2026-01-30 05:52:28 +0000

    -

--- models/T1/customer.sql
+++ models/T1/customer.sql
@@ -9,7 +9,7 @@
 {{
     config(
         materialized="incremental",
-        unique_key=['order_id']
+        unique_key=['order_id'],
         alias="NEW_TABLE_NAME",
         tags=['crm'],
         docs={'node_color': 'red'},
